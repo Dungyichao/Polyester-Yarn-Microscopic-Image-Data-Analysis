36a12d694f6b92d2427aa8eda66587759b303255
Dung-Yi <dungyichao@gmail.com>
2018-12-11 11:51:31 -0500

    Add files via upload

---
+++ QCRuler/Matrix.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace QCRuler
+{
+    public static class Matrix
+    {
+        public static double[,] Laplacian3x3
+        {
+            get
+            {
+                return new double[,]  
+                { { -1, -1, -1,  }, 
+                  { -1,  8, -1,  }, 
+                  { -1, -1, -1,  }, };
+            }
+        }
+
+        public static double[,] Laplacian5x5
+        {
+            get
+            {
+                return new double[,] 
+                { { -1, -1, -1, -1, -1, }, 
+                  { -1, -1, -1, -1, -1, }, 
+                  { -1, -1, 24, -1, -1, }, 
+                  { -1, -1, -1, -1, -1, }, 
+                  { -1, -1, -1, -1, -1  }, };
+            }
+        }
+
+        public static double[,] LaplacianOfGaussian
+        {
+            get
+            {
+                return new double[,]  
+                { {  0,   0, -1,  0,  0 }, 
+                  {  0,  -1, -2, -1,  0 }, 
+                  { -1,  -2, 16, -2, -1 },
+                  {  0,  -1, -2, -1,  0 },
+                  {  0,   0, -1,  0,  0 }, };
+            }
+        }
+
+        public static double[,] Gaussian3x3
+        {
+            get
+            {
+                return new double[,]  
+                { { 1, 2, 1, }, 
+                  { 2, 4, 2, }, 
+                  { 1, 2, 1, }, };
+            }
+        }
+
+        public static double[,] Gaussian5x5Type1
+        {
+            get
+            {
+                return new double[,]  
+                { { 2, 04, 05, 04, 2 }, 
+                  { 4, 09, 12, 09, 4 }, 
+                  { 5, 12, 15, 12, 5 },
+                  { 4, 09, 12, 09, 4 },
+                  { 2, 04, 05, 04, 2 }, };
+            }
+        }
+
+        public static double[,] Gaussian5x5Type2
+        {
+            get
+            {
+                return new double[,] 
+                { {  1,   4,  6,  4,  1 }, 
+                  {  4,  16, 24, 16,  4 }, 
+                  {  6,  24, 36, 24,  6 },
+                  {  4,  16, 24, 16,  4 },
+                  {  1,   4,  6,  4,  1 }, };
+            }
+        }
+
+        public static double[,] Sobel3x3Horizontal
+        {
+            get
+            {
+                //return new double[,] 
+                //{ { -1,  0,  1, }, 
+                //  { -2,  0,  2, }, 
+                //  { -1,  0,  1, }, };
+
+                return new double[,] 
+                { { -1.5,  0,  1.5, }, 
+                  { -3,  0,  3, }, 
+                  { -1.5,  0,  1.5, }, };
+            }
+        }
+
+        public static double[,] Sobel3x3Vertical
+        {
+            get
+            {
+                //return new double[,] 
+                //{ {  1,  2,  1, }, 
+                //  {  0,  0,  0, }, 
+                //  { -1, -2, -1, }, };
+
+                return new double[,] 
+                { {  1.5,  3,  1.5, }, 
+                  {  0,  0,  0, }, 
+                  { -1.5, -3, -1.5, }, };
+            }
+        }
+
+        public static double[,] Prewitt3x3Horizontal
+        {
+            get
+            {
+                return new double[,] 
+                { { -1,  0,  1, }, 
+                  { -1,  0,  1, }, 
+                  { -1,  0,  1, }, };
+            }
+        }
+
+        public static double[,] Prewitt3x3Vertical
+        {
+            get
+            {
+                return new double[,] 
+                { {  1,  1,  1, }, 
+                  {  0,  0,  0, }, 
+                  { -1, -1, -1, }, };
+            }
+        }
+
+
+        public static double[,] Kirsch3x3Horizontal
+        {
+            get
+            {
+                return new double[,] 
+                { {  5,  5,  5, }, 
+                  { -3,  0, -3, }, 
+                  { -3, -3, -3, }, };
+            }
+        }
+
+        public static double[,] Kirsch3x3Vertical
+        {
+            get
+            {
+                return new double[,] 
+                { {  5, -3, -3, }, 
+                  {  5,  0, -3, }, 
+                  {  5, -3, -3, }, };
+            }
+        }
+
+
+        public static double[,] GaussianBlur(int lenght, double weight)
+        {
+            double[,] kernel = new double[lenght, lenght];
+            double kernelSum = 0;
+            int foff = (lenght - 1) / 2;
+            double distance = 0;
+            double constant = 1d / (2 * Math.PI * weight * weight);
+            for (int y = -foff; y <= foff; y++)
+            {
+                for (int x = -foff; x <= foff; x++)
+                {
+                    distance = ((y * y) + (x * x)) / (2 * weight * weight);
+                    kernel[y + foff, x + foff] = constant * Math.Exp(-distance);
+                    kernelSum += kernel[y + foff, x + foff];
+                }
+            }
+            for (int y = 0; y < lenght; y++)
+            {
+                for (int x = 0; x < lenght; x++)
+                {
+                    kernel[y, x] = kernel[y, x] * 1d / kernelSum;
+                }
+            }
+            return kernel;
+         }
+
+    }
+}
